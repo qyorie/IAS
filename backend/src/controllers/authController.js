07da677cf9a51743d830fea7cf7f64141c182b55
Troy Tristan Jacob <qyorie.0707@gmail.com>
2025-11-07 11:22:27 +0800

    added other dependencies, authControllers, and User.js

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await User.create({ username, email, password });
+    res.status(201).json({ message: "User registered successfully!" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ message: "Invalid email" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: "Invalid password" });
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
